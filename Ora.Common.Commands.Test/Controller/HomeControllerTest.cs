5fafb207e854d9b14226a85f2655daec5e56aecc
rajabalianomid:dimoaro6631@ <rajabalian.omid@LIVE.COM>
2020-03-19 01:25:33 +0100

    Unit Test

---
+++ Ora.Common.Commands.Test/Controller/HomeControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using Ora.Services.Configurable;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ora.Common.Commands.Test.Controller
+{
+    public class HomeControllerTest
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public HomeControllerTest()
+        {
+            _server = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task home_controller_test_should_return_string_content()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            content.Should().BeEquivalentTo("Ora.Services.Configurable Is Active");
+        }
+    }
+}
---
+++ Ora.Common.Commands.Test/Service/ConfigServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Options;
+using Mongo2Go;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using Moq;
+using Ora.Common.Caching;
+using Ora.Common.Mongo;
+using Ora.Services.Configurable;
+using Ora.Services.Configurable.Data;
+using Ora.Services.Configurable.Domain;
+using Ora.Services.Configurable.Repository;
+using Ora.Services.Configurable.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ora.Common.Commands.Test.Service
+{
+    public class ConfigServiceTest
+    {
+        [Fact]
+        public async Task activity_service_add_async_should_succeed()
+        {
+            var cashManagerMock = new Mock<IStaticCacheManager>();
+            var _runner = MongoDbRunner.Start();
+            var _client = new MongoClient(_runner.ConnectionString);
+            var _fakeDb = _client.GetDatabase("IntegrationTestDb");
+            var mongoRepository = new MongoRepository<Setting>(_fakeDb);
+            var activityService = new SettingService(mongoRepository, cashManagerMock.Object);
+            await activityService.InsertSetting(new Setting
+            {
+                ApplicationName = "Ora",
+                ChangeUTC = DateTime.UtcNow,
+                CreateUTC = DateTime.UtcNow,
+                Id = 1,
+                IsActive = true,
+                Name = "Omid",
+                Type = "String",
+                Value = "1"
+            });
+            Assert.True(mongoRepository.Table.FirstOrDefault().IsActive);
+        }
+    }
+}
